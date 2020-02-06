4fb1a8911d654ab36d385f4fec742fb68aefd704
Kawakami <Haruki.Kawakami@sony.com>
2020-02-06 19:54:39 +0900

    add google onsite second problem and implementation

--- google/2020_01/src/main/java/onsite/second/Main.java
+++ google/2020_01/src/main/java/onsite/second/Main.java
@@ -6,12 +6,67 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        solve(System.in, System.out);
     }
 
-    static void solve(InputStream is, PrintStream os) {
+    static int lastPos(String[][] input, int startPos) {
         // Your code here!
-        Scanner scan = new Scanner(is);
+        for(int i = 0; i < input.length; i++) {
+            startPos = nextPos(input[i], startPos);
+        }
+        return startPos;
+    }
 
+    static int nextPos(String[] input, int startPos) {
+        if(0 < startPos-1 && startPos-1 < input.length && input[startPos-1].equals("/")){
+            return startPos-1;
+        }
+        if(startPos < input.length && input[startPos].equals("\\")) {
+            return startPos+1;
+        }
+        return startPos;
+    }
+
+    static int minimizeMaximum(String[][] input, int maxPos, int[] balls) {
+        //mapping startPos and lastPos
+        int[] map = new int[maxPos];
+        for(int i = 0; i < maxPos; i++) {
+            map[i] = lastPos(input, i);
+        }
+
+        int max = 0;
+        for(int i = 0; i < balls.length; i++) {
+            max += balls[i];
+        }
+
+        int r = max;
+        int l = 0;
+        while(l + 1< r){
+            int mid = l + (r-l)/2;
+            if(helper(balls, maxPos, mid, map)){
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        return r;
+    }
+
+    static boolean helper(int[] balls, int maxPos, int maxCap, int[] map) {
+        int pos = 0;
+        int[] container = new int[maxPos];
+        for(int i = 0; i < balls.length; i++) {
+            if(maxCap < balls[i])
+                return false;
+            while(pos < maxPos) {
+                if (container[map[pos]] + balls[i] <= maxCap) {
+                    container[map[pos]] += balls[i];
+                    break;
+                } else
+                    pos++;
+            }
+            if(pos == maxPos)
+                return false;
+        }
+        return true;
     }
 }
--- google/2020_01/src/test/java/onsite/second/MainTest.java
+++ google/2020_01/src/test/java/onsite/second/MainTest.java
@@ -2,28 +2,23 @@ package onsite.second;
 
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 public class MainTest {
 
     @Test
-    public void test1() {
-        String input = "test input";
-        String expected = "answer";
-
-        assertThat(execute(input + "\n"), is(expected + "\n"));
+    public void lastPos() {
+      String[][] input = {{"/", "\\", "\\","/", "\\", "\\","/","/","/","/"},{"/", "\\","/", "\\", "\\", "\\"},{"/", "\\","/", "\\", "\\", "\\","/", "\\", "\\", "\\"}};
+      int startPos = 4;
+      int ans = Main.lastPos(input, startPos);
+      System.out.println(ans);
     }
 
-    private String execute(String input) {
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        Main.solve(
-                new ByteArrayInputStream(input.getBytes()), new PrintStream(os)
-        );
-        return os.toString().replace(System.lineSeparator(), "\n");
+    @Test
+    public void minimizeMaximum() {
+      String[][] input = {{"/", "\\", "\\","/", "\\", "\\","/","/","/","/"},{"/", "\\","/", "\\", "\\", "\\"},{"/", "\\","/", "\\", "\\", "\\","/", "\\", "\\", "\\"}};
+      int[] balls = {3,5,7,8,4,2,13,5,7,8,3,9,7,4,10,2};
+      int maxPos = 11;
+      int max = Main.minimizeMaximum(input, maxPos, balls);
+      System.out.println(max);
     }
+
 }
